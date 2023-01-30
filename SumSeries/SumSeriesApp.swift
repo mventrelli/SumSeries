67f5e342ba04855ec333a3ed2166a218b337ddb2
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-01-30 12:25:43 -0600

    Initial Commit

---
+++ SumSeries/SumSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumSeriesApp.swift
+//  SumSeries
+//
+//  Created by IIT PHYS 440 on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SumSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
